d67c12f4b10d83025f2c08cd7c15b90831e2660f
karl <enriquezkharl14@gmail.com>
2025-12-01 18:36:59 +0800

    [Feat] Tenants schema

---
+++ backend/internal/db/migrations/000002_tenants_schema.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS "tenants"(
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "name" VARCHAR(255) NOT NULL,
+    "plan" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMPTZ DEFAULT now()
+);
