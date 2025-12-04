165fe1c4e6d301c0de52caef6713330d5dec3acf
karl <enriquezkharl14@gmail.com>
2025-12-05 00:27:12 +0800

    [Feat] server middlewares

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httprate"
 )
 
 func (app *application) routes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(httprate.LimitByIP(100, time.Minute))
 
 	r.Get("/healthcheck", app.healthCheckHandler)
 
--- backend/go.mod
+++ backend/go.mod
@@ -4,7 +4,11 @@ go 1.25.0
 
 require (
 	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/go-chi/httprate v0.15.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
 )
