099609d555f16558fe652f8da26b6bb33abff218
karl <enriquezkharl14@gmail.com>
2025-12-04 23:36:11 +0800

    [Feat] delete token for user

--- backend/internal/data/tokens.go
+++ backend/internal/data/tokens.go
@@ -52,6 +52,21 @@ func (t TokenModel) Insert(token *Token) error {
 	return err
 }
 
+func (t TokenModel) DeleteAllForUser(scope string, userID uuid.UUID) error {
+	query := `
+		DELETE FROM tokens
+		WHERE scope = $1 AND user_id = $2
+	`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := t.DB.ExecContext(ctx, query, scope, userID)
+
+	return err
+
+}
+
 func generateToken(userID uuid.UUID, ttl time.Duration, scope string) *Token {
 	token := &Token{
 		PlainText: rand.Text(),
