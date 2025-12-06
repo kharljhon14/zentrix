a22ab64abea2e74c44131e1643e6aa1bb26ef49f
karl <enriquezkharl14@gmail.com>
2025-12-06 21:00:22 +0800

    [DB] company schema updates

--- backend/internal/db/migrations/000001_company_schema.up.sql
+++ backend/internal/db/migrations/000001_company_schema.up.sql
@@ -4,9 +4,14 @@ CREATE TABLE "companies"(
     "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "name" VARCHAR(255) NOT NULL,
     "address" VARCHAR(255) NOT NULL,
+    "sales_owner" UUID REFERENCES users(id),
     "email" VARCHAR(255) NOT NULL UNIQUE,
+    "company_size" VARCHAR(255) NOT NULL,
+    "industry" VARCHAR(255) NOT NULL,
+    "business_type" VARCHAR(255) NOT NULL,
+    "country" VARCHAR(255) NOT NULL,
     "image" TEXT,
-    "website" VARCHAR(255),
+    "website" TEXT,
     "created_at" TIMESTAMPTZ DEFAULT now(),
     "updated_at" TIMESTAMPTZ DEFAULT now()
 );
