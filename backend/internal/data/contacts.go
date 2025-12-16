353de343b05ea499c110e1876ff96b37b36ea05c
karl <enriquezkharl14@gmail.com>
2025-12-17 02:25:59 +0800

    [Feat] Get contacy by id

--- backend/internal/data/contacts.go
+++ backend/internal/data/contacts.go
@@ -60,3 +60,54 @@ func (c ContactModel) Insert(contact Contact) error {
 
 	return nil
 }
+
+type ContactWithCompanyName struct {
+	ID          uuid.UUID  `json:"uuid"`
+	Name        string     `json:"name"`
+	Email       string     `json:"email"`
+	CompanyID   *uuid.UUID `json:"company_id"`
+	CompanyName *string    `json:"company_name"`
+	Title       string     `json:"title"`
+	Status      string     `json:"status"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+}
+
+func (c ContactModel) GetByIDWithCompanyName(ID uuid.UUID) (*ContactWithCompanyName, error) {
+	query := `
+		SELECT 
+			c.id,
+			c.name,
+			c.email,
+			o.id AS company_id,
+			o.name AS company_name,
+			c.title,
+			c.status,
+			c.created_at,
+			c.updated_at
+		FROM contacts c
+		JOIN companies o
+		WHERE id = $1 AND deleted_at IS NULL
+	`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var contact ContactWithCompanyName
+	err := c.DB.QueryRowContext(ctx, query, ID).Scan(
+		&contact.ID,
+		&contact.Name,
+		&contact.Email,
+		&contact.CompanyID,
+		&contact.CompanyID,
+		&contact.Title,
+		&contact.Status,
+		&contact.CreatedAt,
+		&contact.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &contact, nil
+}
